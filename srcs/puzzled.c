f1f576cf0a808b160177bc10892b19477ba58619
bw-bmbarga <bmbarga@brandwatch.com>
2019-12-16 15:24:11 +0100

    feat: implement getfile function

--- srcs/puzzled.c
+++ srcs/puzzled.c
@@ -6,21 +6,52 @@
 /*             <nleme@live.fr>                                                */
 /*                                                                            */
 /*   Created: Wed Dec 11 16:16:42 2019                        by elhmn        */
-/*   Updated: Mon Dec 16 14:20:02 2019                        by bmbarga      */
+/*   Updated: Mon Dec 16 15:27:51 2019                        by bmbarga      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 #include "puzzled.h"
 
-char **get_dictionnary(char *dict_file) {
-	printf("get dictionnary"); // Debug
-	printf("dict_file : %s\n", dict_file); // Debug
-	return (NULL);
+char *getfile(char *dict_file) {
+	FILE *fp = NULL;
+	char *dict = NULL;
+	long fsize = 0;
+
+	if (!(fp = fopen(dict_file, "r"))) {
+		perror("Error: fopen: ");
+		exit(EXIT_FAILURE);
+	}
+
+	//Get file size
+	fseek(fp, 0, SEEK_END);
+	fsize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	//create dict string
+	if (!(dict = (char*)malloc(fsize + 1))) {
+		return (NULL);
+	}
+
+	//This call is only
+	fread(dict, 1, fsize, fp);
+
+	fclose(fp);
+	return (dict);
 }
 
 int puzzled(int n, int m, char *dict_file) {
+	char *dict = NULL;
+
 	printf("puzzled is running with N = [%d] && M = [%d]\n", n, m); // Debug
-	get_dictionnary(dict_file);
+
+	if (!(dict = getfile(dict_file))) {
+		printf("Error: dict: set to NULL");
+		return (-1);
+	}
+
 	return (0);
 }
